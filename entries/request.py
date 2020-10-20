b9f2e4e1843cde6083aad3209de6daad4a9a8fca
LukeZvada <zvadamusic@gmail.com>
2020-10-20 09:07:29 -0700

    removed comment

--- entries/request.py
+++ entries/request.py
@@ -94,34 +94,3 @@ def delete_entry(entryId):
 		FROM EntryTags AS et
 		WHERE et.entry_id = ?
 		""", ( entryId, ))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Hey team! I will be handling the data collection to update our class JSON file. All I really need from you is to use the following template. Don't worry about the Img fields, and if you don't have the personal site complete leave that blank as well.
-# Respond to this thread with the data, and I will take it from there. Deadline to get this info to me (except the portfolio site which can be left blank for now) will be 27 October. Please complete this whenever you get a moment, thanks!
-# {
-#             "id": 9,
-#             "firstName": "Luke",
-#             "lastName": "Zvada",
-#             "reelThemIn": "An easy solution",
-#             "bio": "Previously working in the music industry as a tour manager, I learned how to solve issues on the go, organize a complicated schedule, and manage team morale. Through that hard work, I have found a new passion for software development. I attended Nashville Software School that allowed me to take that new passion and turn it into reality. Having strong attributes such as problem solving, dependability, and efficiency, I have embraced this new opportunity in tech and continue to learn and grow as a software developer every day. ",
-#             "github": "https://github.com/LukeZvada",
-#             "linkedIn": "https://www.linkedin.com/in/lukezvada/",
-#             "portfolio": "",
-#             "email": "luke.zvada@gmail.com",
-#             "proImg": "",
-#             "funImg": ""
-#         }
